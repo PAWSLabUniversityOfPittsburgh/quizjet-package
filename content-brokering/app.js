6fdfaaf7f25429009d7dd04f1ea803305aa04892
hardikvasa <hnvasa@gmail.com>
2016-03-27 23:35:33 -0400

    Content Brokering API update

--- content-brokering/app.js
+++ content-brokering/app.js
@@ -188,94 +188,135 @@ app.get('/', function(req, res) {
 // ********************************************************************************
 // API
 
-var api = express.Router();
-app.use('/api/v1', api);
+var _ = require('lodash');
 
-api.get('/', function(req, res) {
-    console.log('started!');
-    var urls = {
-        content: { url: config.serverAddress + 'api/v1/content', description: 'Lists all available content types'}
-        // search: {url: config.serverAddress + 'api/v1/search', description: 'Search'}
-    };
+// These fields will be in the response, if the field is available
+var fields = ['name', 'title', 'description', 'author', 'license', 'version', 'created', 'modified', 'keywords'];
 
-    res.json(urls);
-
-});
-
-//Has all content types
-api.get('/content', function(req, res) {
-    console.log('again');
+var contentTypesAsJSON = function(filters, expand) {
     var contentTypes = [];
     var allContentTypes = handlers.contentTypes;
 
     Object.keys(allContentTypes).forEach(function(key) {
+
+        if (filters.keyword && allContentTypes[key].meta.keywords.indexOf(filters.keyword) < 0) {
+            return;
+        }
+
+        if (filters.author && (allContentTypes[key].meta.author || '') !== filters.author) {
+            return;
+        }
+
         var currentContentType = {};
+
+        fields.forEach(function(field) {
+            if (allContentTypes[key].meta[field]) {
+                currentContentType[field] = allContentTypes[key].meta[field];
+            }
+        });
+
         currentContentType.name = allContentTypes[key].meta.name;
-        console.log(currentContentType.name);
+        //console.log(currentContentType.name);
         currentContentType.url = config.serverAddress +'api/v1/content/' + allContentTypes[key].meta.name;
         currentContentType.title = allContentTypes[key].meta.title;
         currentContentType.description = allContentTypes[key].meta.shortDescription;
         currentContentType.author = allContentTypes[key].meta.author;
         currentContentType.license = allContentTypes[key].meta.license;
         currentContentType.version = allContentTypes[key].meta.version;
+
+        if (expand) {
+            currentContentType.subitems = contentPackagesAsJSON(key, filters, true);
+        }
+
         contentTypes.push(currentContentType);
+
     });
 
-    res.json({contentTypes: contentTypes});
+    return contentTypes;
 
-});
+};
 
-// Has all content packages in the given content type
-api.get('/content/:contentType([a-z]+)', function(req, res) {
-
-    if (req.params.contentType in handlers.contentTypes) {
-
-        var packages = handlers.contentTypes[req.params.contentType].installedContentPackages;
-        //console.log(packages);
+var contentPackagesAsJSON = function(contentType, filters, expand) {
+    if (handlers.contentTypes[contentType]) {
         var contentPackages = [];
+        var packages = handlers.contentTypes[contentType].installedContentPackages;
         var names = ['java','python','sql','c','cpp','vb','name'];
         for (var i = 0; i < packages.length; i++) {
-            var currentContent = {};
-            currentContent.name = packages[i].meta[names[i]];
-            currentContent.url = config.serverAddress + 'api/v1/content/' + req.params.contentType + '/' + packages[i].meta[names[i]];
-            currentContent.title = packages[i].meta.title;
-            currentContent.description = packages[i].meta.shortDescription;
-            currentContent.author = packages[i].meta.author;
-            currentContent.license = packages[i].meta.license;
-            currentContent.version = packages[i].meta.version;
-            currentContent.created = packages[i].meta.created;
-            currentContent.modified = packages[i].meta.modified;
-            currentContent.keywords = packages[i].meta.keywords;
-            contentPackages.push(currentContent);
-        }
-        //console.log(contentPackages);
-        res.json({contentPackages: contentPackages});
 
+            if (filters.keyword && package.meta.keywords.indexOf(filters.keyword) < 0) {
+                return;
+            }
+
+            if (filters.author && package.meta.author !== filters.author) {
+                return;
+            }
+
+            var currentPackage = {};
+
+            fields.forEach(function(field) {
+                if (package.meta[field]) {
+                    currentPackage[field] = package.meta[field];
+                }
+            });
+
+            currentPackage.url = config.serverAddress + 'api/v1/content/' + contentType + '/' + package.meta.name;
+            currentPackage.name = packages[i].meta[names[i]];
+            currentPackage.url = config.serverAddress + 'api/v1/content/' + contentType + '/' + packages[i].meta[names[i]];
+            currentPackage.title = packages[i].meta.title;
+            currentPackage.description = packages[i].meta.shortDescription;
+            currentPackage.author = packages[i].meta.author;
+            currentPackage.license = packages[i].meta.license;
+            currentPackage.version = packages[i].meta.version;
+            currentPackage.created = packages[i].meta.created;
+            currentPackage.modified = packages[i].meta.modified;
+            currentPackage.keywords = packages[i].meta.keywords;
+
+            if (expand) {
+                currentPackage.subitems = contentPackageAsJSON(contentType, package.meta.name, filters, true, false);
+            }
+
+            contentPackages.push(currentPackage);
+        };
+        return contentPackages;
     } else {
-        res.status(404).json({'error':'Unknown content type.'});
+        return null;
     }
+};
 
-});
+var contentPackageAsJSON = function(contentType, contentPackage, filters, expand, expandProtocolUrls) {
+    if (handlers.contentPackages[contentPackage]) {
 
-// All exercises in the given content package
-api.get('/content/:contentType([a-z]+)/:contentPackage([a-zA-Z0-9_-]+)', function(req, res) {
-
-    if (req.params.contentPackage in handlers.contentPackages) {
-        //console.log(handlers.contentPackages);
-        var namee = (req.params.contentPackage).replace('-','_');
+        var namee = (contentPackage).replace('-','_');
         console.log(namee);
-        var allContent = handlers.contentPackages[req.params.contentPackage].meta[namee];
-        //console.log(allContent);
+        var allContent = handlers.contentPackages[contentPackage].meta[namee];
+
+        //var allContent = handlers.contentPackages[contentPackage].meta.contents || [];
         var content = [];
 
         Object.keys(allContent).forEach(function(key) {
+
+            if (filters.keyword && (allContent[key].keywords || []).indexOf(filters.keyword) < 0) {
+                return;
+            }
+
+            if (filters.author && (allContent[key].author || handlers.contentPackages[contentPackage].meta.author) !== filters.author) {
+                return;
+            }
+
             var currentContent = {};
+
+            fields.forEach(function(field) {
+                if (allContent[key][field]) {
+                    currentContent[field] = allContent[key][field];
+                }
+            });
+
             currentContent.name = key;
             currentContent.title = allContent[key].title;
             currentContent.description = allContent[key].description;
-            currentContent.url = config.serverAddress + 'api/v1/content/' + req.params.contentType + '/' + req.params.contentPackage + '/' + currentContent.name;
-            if (req.params.contentType == 'annotated') {
-                currentContent.html_url = config.serverAddress + 'pitt/' + req.params.contentType + '/' + req.params.contentPackage + '/' + 'Dissection2?act='+ key;
+            currentContent.url = config.serverAddress + 'api/v1/content/' + contentType + '/' + contentPackage + '/' + currentContent.name;
+            if (contentType == 'annotated') {
+                currentContent.html_url = config.serverAddress + 'pitt/' + contentType + '/' + contentPackage + '/' + 'Dissection2?act='+ key;
             }
             else{
                 currentContent.html_url = 'http://adapt2.sis.pitt.edu/quizjet/' + 'quiz1.jsp?rdfID='+ key;
@@ -284,66 +325,146 @@ api.get('/content/:contentType([a-z]+)/:contentPackage([a-zA-Z0-9_-]+)', functio
             currentContent.version = allContent[key].version;
             currentContent.created = allContent[key].created;
             currentContent.modified = allContent[key].modified;
+            currentContent.author_id = allContent[key].author_id;
             currentContent.keywords = allContent[key].keywords;
-            //currentContent.subitems = allContent[key].subitems;
 
-            if (req.query.expand && req.query.expand.split(',').indexOf('protocol_urls') > -1) {
+            if (expandProtocolUrls) {
                 currentContent.protocol_urls = {};
                 for (var protocol in handlers.protocols) {
-                    currentContent.protocol_urls[protocol] = config.serverAddress + protocol + '/' + req.params.contentType + '/' + req.params.contentPackage + '/' + currentContent.name;
+                    currentContent.protocol_urls[protocol] = config.serverAddress + protocol + '/' + contentType + '/' + contentPackage + '/' + currentContent.name;
                 }
             }
+
+            /*if (expand) {
+             currentContent.subitems = contentAsJSON(contentType, contentPackage, currentContent.name, filters, true, false);
+             }*/
 
             content.push(currentContent);
         });
 
-        res.json({content: content});
+        return content;
 
     } else {
-        res.status(404).json({'error':'Unknown content package.'});
+        return null;
     }
+};
 
-});
+var contentAsJSON = function(contentType, contentPackage, content, filters, expand, expandProtocolUrls) {
+    var nameee = (contentPackage).replace('-','_');
+    if (handlers.contentPackages[contentPackage] && handlers.contentPackages[contentPackage].meta[nameee]) {
 
-// Information about the given exercise
-api.get('/content/:contentType([a-z]+)/:contentPackage([a-zA-Z0-9_-]+)/:name([a-zA-Z0-9_.-]+)', function(req, res) {
-    var nameee = (req.params.contentPackage).replace('-','_');
-    if (req.params.contentPackage in handlers.contentPackages && req.params.name in handlers.contentPackages[req.params.contentPackage].meta[nameee]) {
-        console.log(true);
-        var exercise = handlers.contentPackages[req.params.contentPackage].meta[nameee][req.params.name];
+        var exercise = handlers.contentPackages[contentPackage].meta[nameee][content];
+        console.log(exercise);
 
         var exerciseInfo = {};
-        exerciseInfo.name = req.params.name;
-        exerciseInfo.url = config.serverAddress + 'api/v1/content/' + req.params.contentType + '/' + req.params.contentPackage + '/' + req.params.name;
+        exerciseInfo.name = content;
+        //exerciseInfo.name = req.params.name;
+        exerciseInfo.url = config.serverAddress + 'api/v1/content/' + contentType + '/' + contentPackage + '/' + content;
 
-        if (req.params.contentType == 'annotated') {
-            exerciseInfo.html_url = config.serverAddress + 'pitt/' + req.params.contentType + '/' + req.params.contentPackage + '/' + 'Dissection2?act=' + req.params.name;
+        if (contentType == 'annotated') {
+            exerciseInfo.html_url = config.serverAddress + 'pitt/' + contentType + '/' + contentPackage + '/' + 'Dissection2?act=' + content;
         }
         else{
-            exerciseInfo.html_url = 'http://adapt2.sis.pitt.edu/quizjet/' + 'quiz1.jsp?rdfID='+ req.params.name;
+            exerciseInfo.html_url = 'http://adapt2.sis.pitt.edu/quizjet/' + 'quiz1.jsp?rdfID='+ content;
         }
 
         exerciseInfo.title = exercise.title;
         exerciseInfo.description = exercise.description;
         exerciseInfo.version = exercise.version;
         exerciseInfo.keywords = exercise.keywords;
-        exerciseInfo.subitems = exercise.subitems;
 
-        if (req.query.expand && req.query.expand.split(',').indexOf('protocol_urls') > -1) {
-            exerciseInfo.protocol_urls = {};
-            for (var protocol in handlers.protocols) {
-                exerciseInfo.protocol_urls[protocol] = config.serverAddress + protocol + '/' + req.params.contentType + '/' + req.params.contentPackage + '/' + req.params.name;
+
+        fields.forEach(function(field) {
+            if (exercise[field]) {
+                exerciseInfo[field] = exercise[field];
             }
+        });
+
+        if (expandProtocolUrls) {
+            exerciseInfo.subitems = exercise.subitems;
         }
 
-        res.json({exercise: exerciseInfo});
+        return exerciseInfo;
 
     } else {
-        console.log(false);
-        res.status(404).json({'error':'Unknown exercise.'});
+        return null;
     }
+};
+
+
+var createFilters = function(req) {
+    var filters = {};
+    var allowed = ['keyword', 'author'];
+    allowed.forEach(function(item) {
+        if (req.query[item]) {
+            filters[item] = req.query[item];
+        }
+    });
+    return filters;
+};
+
+// **********************************************
+
+var api = express.Router();
+app.use('/api/v1', api);
+
+api.get('/', function(req, res) {
+
+    var endpoints = {
+        content: { url: config.serverAddress + 'api/v1/content', description: 'Lists all available content types'},
+    };
+
+    res.json(endpoints);
 
 });
+
+// All content types
+api.get('/content', function(req, res) {
+    var expand = req.query.expand && req.query.expand.split(',').indexOf('subitems') > -1;
+    var filters = createFilters(req);
+    var response = {contentTypes: contentTypesAsJSON(filters, expand)};
+    res.json(response);
+});
+
+// All content packages in the given content type
+api.get('/content/:contentType([a-zA-Z0-9_-]+)', function(req, res) {
+    var expand = req.query.expand && req.query.expand.split(',').indexOf('subitems') > -1;
+    var filters = createFilters(req);
+    var response = contentPackagesAsJSON(req.params.contentType, filters, expand);
+    if (response) {
+        res.json({contentPackages: response});
+    } else {
+        res.status(404).json({'error':'Unknown content type.'});
+    }
+});
+
+// All exercises in the given content package
+api.get('/content/:contentType([a-zA-Z0-9_.-]+)/:contentPackage([a-zA-Z0-9_.-]+)', function(req, res) {
+    var expand = req.query.expand && req.query.expand.split(',').indexOf('subitems') > -1;
+    var expandProtocolUrls = req.query.expand && req.query.expand.split(',').indexOf('protocol_urls') > -1;
+    var filters = createFilters(req);
+    var response = contentPackageAsJSON(req.params.contentType, req.params.contentPackage, filters, expand, expandProtocolUrls);
+    if (response) {
+        res.json({content: response});
+    } else {
+        res.status(404).json({'error':'Unknown content package.'});
+    }
+});
+
+// Information about the given exercise
+api.get('/content/:contentType([a-zA-Z0-9_-]+)/:contentPackage([a-zA-Z0-9_-]+)/:name([a-zA-Z0-9_.-]+)', function(req, res) {
+    var expand = req.query.expand && req.query.expand.split(',').indexOf('subitems') > -1;
+    var expandProtocolUrls = req.query.expand && req.query.expand.split(',').indexOf('subitems') > -1;
+    var filters = createFilters(req);
+    var response = contentAsJSON(req.params.contentType, req.params.contentPackage, req.params.name, filters, expand, expandProtocolUrls);
+    if (response) {
+        res.json({item: response});
+    } else {
+        res.status(404).json({'error':'Unknown content item.'});
+    }
+});
+
+
 
 // ********************************************************************************
 // Dummy logger for debugging purposes
