f176df7c4b0e21421075b1f7b0795c6c5b200a98
mallium <mallium@gmail.com>
2016-04-05 17:39:04 -0400

    remove authoring

--- quizjet-authoring/QuizJetAuthoring-master/react component/quizjet/index.jsx
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var React = require('react');
-var ReactDOM = require('reactDom');
-
-var Dashboard = require('./component/Dashboard');
-var URL = require('./json/ApiUrl');
-
-ReactDOM.render(<Dashboard url={URL} />, document.getElementById('app'));
